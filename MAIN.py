d7c2adde843e3f77dd99d906f435ef127dd8def0
123prabh <prabhatrathore.pr@gmail.com>
2023-03-05 13:57:08 +0530

    P

--- MAIN.py
+++ MAIN.py
@@ -3,5 +3,9 @@ import json
 from datetime import datetime, timedelta
 from tkinter import Tk, Label, PhotoImage, messagebox
 
-print("prabhat 123")
-print("anish")
+def _init_(self, api_key):
+        self.api_key = api_key
+        self.city_name = None
+        self.temperature = None
+        self.weather_description = None
+        self.forecast_data = None
